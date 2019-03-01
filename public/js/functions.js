149783a2dacc2f892c792308d7a653cd78b3648a
Aaron Snowberger <jekkilekki@gmail.com>
2019-03-01 11:25:15 +0900

    [feat] Add countdown clock

--- public/js/functions.js
+++ public/js/functions.js
@@ -17,3 +17,114 @@ $( '.benefit-title' ).click( function() {
     $(this).next().slideToggle();
   }
 });
+
+/**
+ * Date Countdown timer
+ * @link https://codepen.io/chrisjdesigner/pen/dMbmoE
+ */
+var winHeight = $(document).height();
+var winWidth = $(document).width();
+var thisYear = new Date();
+var thisChristmas = thisYear.getFullYear() + "/05/25 09:00:00";
+var christmas = new Date( thisChristmas ),
+    days, hours, mins, secs;
+
+$('.this-year').text( thisYear.getFullYear() );
+
+//var setPieChart = function(name) {
+  //var num = name,
+//      fixedNumber = num,
+//      result = fixedNum + ' ' + total;
+//  pie.style.strokeDasharray = 25;
+//}
+
+function showToday() {
+  var date = thisYear.getDate();
+  // var date = 2;
+  $('.day-list li:nth-child(' + date + ')').addClass('today');
+  
+  $('#container #inner').css({"margin-left": -(date-1) * winWidth + "px"});
+  
+  var fgifts = $( '.day-list li' );
+  $.each( fgifts, function( i, obj ) {
+    if( i+1 <= date ) $(this).removeClass('disabled'); 
+    if( i+1 == date ) $(this).addClass( 'today' );
+    if( i+1 > date ) { $(this).addClass('disabled');
+                      $(this).unbind('click');
+    }
+  });
+  
+  $('#posts .post').css({"display":"none"});
+  $('#posts .post:nth-child(' + date + ')').css({"display":"block"});
+}
+
+$( '.disabled' ).click( function(e) {
+  e.preventDefault();
+  alert( 'Cheatin\' huh?' );
+});
+
+$(function() {
+  // Calculate time to Christmas
+  showToday();
+  timeToXmas();
+  // Transition from 0
+  numberTrans( '#days .number', days, 1000, 'easeOutQuad' );
+  numberTrans( '#hours .number', hours, 1000, 'easeOutQuad' );
+  numberTrans( '#minutes .number', mins, 1000, 'easeOutQuad' );
+  numberTrans( '#seconds .number', secs, 1000, 'easeOutQuad' );
+  // Begin countdown
+  setTimeout( countdownTimer, 1001 );
+});
+
+// function to calc Time to Christmas
+function timeToXmas() {
+  var today = new Date(); 
+  // diff between dates
+  var diff = (today - christmas)/1000;
+  var diff = Math.abs(Math.floor(diff));
+  
+  // Day to target
+  days = Math.floor(diff/(24*60*60));
+  secs = diff - days * 24*60*60;
+  // Hours
+  hours = Math.floor(secs/(60*60));
+  secs = secs - hours * 60*60;
+  // Minutes
+  mins = Math.floor(secs/60);
+  secs = secs - mins * 60;
+}
+
+// function to display the countdown Timer
+function countdownTimer() {
+  timeToXmas();
+  // display in front-end clock
+  // Seems like multiplying by 360(degrees) gives an inaccurate measure - 315 is the proper number to be an even division of the pie graph clock
+  $( '#days .number' ).text(days);
+  var dayResult = 315*(days/25) + ' ' + 360;
+  $( '#days .pie' ).css( { "stroke-dasharray": dayResult } );
+  $( '#hours .number' ).text(hours);
+  var hrResult = 315*(hours/24) + ' ' + 360;
+  $( '#hours .pie' ).css( { "stroke-dasharray": hrResult } );
+  $( '#minutes .number' ).text(mins);
+  var minResult = 315*(mins/60) + ' ' + 360;
+  $( '#minutes .pie' ).css( { "stroke-dasharray": minResult } );
+  $( '#seconds .number' ).text(secs);
+  var secResult = 315*(secs/60) + ' ' + 360;
+  $( '#seconds .pie' ).css( { "stroke-dasharray": secResult } );
+  // repeat every second
+  setTimeout(countdownTimer, 1000);
+}
+
+// Transition numbers
+function numberTrans( id, endpt, transDur, transEase ) {
+  $({numberCount: $(id).text()}).animate({numberCount: endpt}, {
+    duration: transDur,
+    easing: transEase,
+    step: function() {
+      $(id).text(Math.floor(this.numberCount));
+    },
+    complete: function() {
+      $(id).text(this.numberCount);
+    }
+  });
+};
